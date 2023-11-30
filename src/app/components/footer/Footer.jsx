500e83ad75f03e1b3b90cd6a455e35549dcf1e75
Gabodze <mgabodze96@gmail.com>
2023-11-30 12:29:10 +0400

    Comments are cleared

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -7,8 +7,8 @@ import Image from 'next/image'
 function Footer(){
     return (
        <div className={styles.container}> 
-       {/*Hellogi */}
-       {/*Hello */}
+     
+
         <div>@all rights</div>
         <div className={styles.sotialNetworks}>
             <img src="/1.png"  className={styles.icon}
